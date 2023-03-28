6d1f7d3cbcfe1d350868e53098b713ce7b93e70a
Chocolate covered Strawberry <75491899+lhpark114@users.noreply.github.com>
2023-03-27 23:11:17 -0400

    ChannelInfo test

--- src/components/tests/ChannelInfo.test.js
+++ src/components/tests/ChannelInfo.test.js
@@ -10,6 +10,21 @@ describe('ChannelInfo', () => {
     afterEach(() => fakeYoutube.channelImageURL.mockReset());
     it('renders correctly', async () => {
         fakeYoutube.channelImageURL.mockImplementation(() => 'url');
+        const { asFragment } = render(
+            withAllContexts(
+                withRouter(
+                <Route path='/' element={<ChannelInfo id='id' name='channel' />} />
+                ),
+                fakeYoutube
+                ));
+                await waitFor(() => screen.getByRole('img'));
+                expect(asFragment()).toMatchSnapshot();
+    });
+
+    it('renders without URL', () => {
+        fakeYoutube.channelImageURL.mockImplementation(() => {
+            throw new Error('error');
+        });
         render(
             withAllContexts(
                 withRouter(
@@ -17,6 +32,19 @@ describe('ChannelInfo', () => {
                 ),
                 fakeYoutube
                 ));
-                await waitFor(() => screen.getByText('channel'));
+                expect(screen.queryByRole('img')).toBeNull();
+    });
+
+    it('renders with URL', async () => {
+        fakeYoutube.channelImageURL.mockImplementation(() => 'url');
+        render(
+            withAllContexts(
+                withRouter(
+                <Route path='/' element={<ChannelInfo id='id' name='channel' />} />
+                ),
+                fakeYoutube
+                ));
+                await waitFor(() => expect(screen.queryByRole('img')).toBeInTheDocument());
     });
 });
+
