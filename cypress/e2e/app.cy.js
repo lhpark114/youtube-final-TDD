655e554621068449f4868a3cce4bd775d34cc325
Chocolate covered Strawberry <75491899+lhpark114@users.noreply.github.com>
2023-03-28 22:13:50 -0400

    E2E : app.cy.js - json

--- cypress/e2e/app.cy.js
+++ cypress/e2e/app.cy.js
@@ -1,12 +1,23 @@
 /// <reference types="cypress" />
-import '@testing-library/cypress/add-commands'
+import '@testing-library/cypress/add-commands';
 
 describe('Youtube App', () => {
     beforeEach(() => {
+        cy.intercept('GET', /(mostPopular)/g, {
+            fixture: 'popular.json',
+        });
+        cy.intercept('GET', /(search)/g, {
+            fixture: 'search.json',
+        });
+        cy.viewport(1200, 800);
         cy.visit('/');
     });
 
     it('renders', () => {
         cy.findByText('Youtube').should('exist');
+    });
+
+    it('shows popular video first', () => {
+        cy.findByTestId('Popular video').should('exist');
     });
 });
